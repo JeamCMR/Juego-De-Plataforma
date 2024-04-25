ca75ad5e446279bd9c15eb3dad102702a8a2324c
COPSTA <tutomx16@gmail.com>
2024-04-25 14:36:42 -0500

    Declara e inicializa variables de la Api canvas, se crea funcion adaptiva

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ const startScreen = document.querySelector(".start-screen");
 const checkpointScreen = document.querySelector(".checkpoint-screen");
 
 const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+
+const ctx = canvas.getContext("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+const gravity = 0.5;
+let isCheckpointCollisionDetectionActive  = true;
+
+
+const proportionalSize = (size) => {}
