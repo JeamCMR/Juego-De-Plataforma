c733fb7eb5d0b374e23935c1050c02a8e62109b3
COPSTA <tutomx16@gmail.com>
2024-04-25 14:23:46 -0500

    Declara e inicializa variables necesarias para manipular el DOM

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*VARIABLES */
+
+const startBtn = document.getElementById("start-btn");
+const canvas = document.getElementById("canvas");
+
+const startScreen = document.querySelector(".start-screen");
+const checkpointScreen = document.querySelector(".checkpoint-screen");
+
+const checkpointMessage = document.querySelector(".checkpoint-screen > p");
