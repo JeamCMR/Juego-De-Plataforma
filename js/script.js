6fe95df10603362e1f08467c4c1b5e78db7bbce4
COPSTA <tutomx16@gmail.com>
2024-04-25 17:02:42 -0500

    Agrega la clase player con sus propiedades  y atributos

--- js/script.js
+++ js/script.js
@@ -15,4 +15,25 @@ const gravity = 0.5;
 let isCheckpointCollisionDetectionActive  = true;
 
 
-const proportionalSize = (size) => {}
+const proportionalSize = (size) => {
+    return  innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
+}
+
+class Player {
+    constructor(){
+        this.position = {
+            x:proportionalSize(10),
+            y:proportionalSize(400),
+        };
+        this.velocity = {
+            x:0,
+            y:0
+        };
+        this.width = proportionalSize(40);
+        this.height = proportionalSize(40);
+    };
+    
+    draw(){
+
+    }
+}
