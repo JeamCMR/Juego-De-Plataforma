7a399b308e0993dfb911835a05260301a0dc2443
COPSTA <tutomx16@gmail.com>
2024-04-26 18:47:47 -0500

    Agrega logica para mostrar los mensajes cuando se toca un checkPoint

--- js/script.js
+++ js/script.js
@@ -178,7 +178,26 @@ const animate = () =>{
         player.velocity.y = gravity;
       }
     });
-
+    checkpoints.forEach((checkpoint, index,checkpoints)=>{
+      const checkpointDetectionRules  = [
+        player.position.x >= checkpoint.position.x,
+        player.position.y >= checkpoint.position.y,
+        player.position.y + player.height <= checkpoint.position.y + checkpoint.height,
+        isCheckpointCollisionDetectionActive,
+        player.position.x - player.width <= checkpoint.position.x - checkpoint.width + player.width * 0.9,
+        index === 0 || checkpoints[index - 1].claimed === true
+      ];
+      if(checkpointDetectionRules.every(rule => rule)){
+        checkpoint.claim();
+        if(index === checkpoints.length-1){
+          isCheckpointCollisionDetectionActive = false;
+          showCheckpointScreen("You reached the final checkpoint!");
+          movePlayer("ArrowRight",0,false);
+        }else if(player.position.x >= checkpoint.position.x && player.position.x <= checkpoint.position.x + 40){
+          showCheckpointScreen("You reached a checkpoint!");
+        };
+      };
+    });
 }
 
 //Letras a presionar
