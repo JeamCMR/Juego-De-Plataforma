862beccc7461dd3f831210356512331417fcf21f
COPSTA <tutomx16@gmail.com>
2024-04-26 14:14:01 -0500

    Agrega funcion de movimiento y objeto con las letras a presionar

--- js/script.js
+++ js/script.js
@@ -64,6 +64,30 @@ class Player {
 //Se crear una instancia de la clase 
 const player = new Player();
 
+
+//Funcion de movimiento del jugador
+
+const animate = () =>{
+    requestAnimationFrame(animate);
+    ctx .clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    if (!keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
+        player.velocity.x = 5;
+    }else if (!keys.leftKey.pressed && player.position.x > proportionalSize(100)){
+        player.velocity.x = -5;
+    }else {player.velocity.x = 0;} 
+}
+
+
+const keys = {
+    rightKey:{
+        pressed: false
+    },
+    leftKey:{
+        pressed: false
+    }
+}
+
 //Funcion para iniciar el juego
 const startGame = () =>{
     canvas.style.display = "block";
