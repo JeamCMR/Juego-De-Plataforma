d5177ac4e3bcef9c42bb0ab718858da5273d3edb
COPSTA <tutomx16@gmail.com>
2024-04-26 17:59:20 -0500

    Agrega logica para colisicion del jugador con la plataformas

--- js/script.js
+++ js/script.js
@@ -97,7 +97,6 @@ const platformPositions = [
 ]
 
 const platforms  = platformPositions.map(platform => new Platform(platform.x , platform.y));
-console.log(platforms);
 //Funcion de movimiento del jugador
 
 const animate = () =>{
@@ -118,7 +117,29 @@ const animate = () =>{
         platforms.forEach(platform =>  {platform.position.x -= 5});
     }else if(keys.leftKey.pressed && isCheckpointCollisionDetectionActive){
         platforms.forEach((platform)=>{ platform.position.x += 5});
+    }
+
+    platforms.forEach((platform) => {
+      const collisionDetectionRules = [player.position.y + player.height <= platform.position.y,
+      player.position.y + player.height + player.velocity.y >= platform.position.y,
+      player.position.x >= platform.position.x - player.width/2,
+      player.position.x <= platform.position.x + platform.width - player.width /3 ];
+      if (collisionDetectionRules.every(rule => rule) ) {
+        player.velocity.y = 0;
+        return;
       }
+      const platformDetectionRules = [
+        player.position.x >= platform.position.x - player.width / 2,
+        player.position.x <=  platform.position.x + platform.width - player.width / 3,
+        player.position.y + player.height >= platform.position.y,
+        player.position.y <= platform.position.y + platform.height
+      ];
+      if (platformDetectionRules.every(rule => rule)) {
+        player.position.y = platform.position.y + player.height
+        player.velocity.y = gravity;
+      }
+    });
+
 }
 
 //Letras a presionar
